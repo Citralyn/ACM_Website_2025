d3538dfd48ef3f4c6a7af90e28a85d25d714f291
Alexa <alexaduffy1414@gmail.com>
2025-04-12 11:34:18 -0700

    more error checking

--- nextjs-acm_2025/next.config.ts
+++ nextjs-acm_2025/next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  basePath: '/ACM_Website_2025',
-  output: 'export'
+  basePath: '/ACM_Website_2025'
 };
 
 export default nextConfig;
