51bc04d4ed3c604fd2bdccb72e41bca7534fe179
Alexa <alexaduffy1414@gmail.com>
2025-04-12 12:24:15 -0700

    temporary vite directory

--- nextjs-acm_2025/next.config.ts
+++ nextjs-acm_2025/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/ACM_Website_2025'
+  basePath: '/ACM_Website_2025',
+  output: 'export',
+  distDir: 'out'
 };
 
 export default nextConfig;
