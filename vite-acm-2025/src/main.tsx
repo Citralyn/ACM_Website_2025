f4e109e9d30adad39ef1d7600b126aa698bf03bc
Alexa <alexaduffy1414@gmail.com>
2025-04-13 16:41:02 -0700

    added a temporary navbar to make sure the hashrouter works ok, will have to provide documentation soon

--- vite-acm-2025/src/main.tsx
+++ vite-acm-2025/src/main.tsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { HashRouter, Routes, Route } from "react-router";
 import './index.css'
+
+import MyNav from "./components/navbar.tsx"
 import App from './App.tsx'
+import Extra from "./pages/extra.tsx"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HashRouter>
+    <Routes>
+    <Route path="/" element={<MyNav />}>
+      <Route index element={<App/>}></Route>
+      <Route path="strange" element={<Extra/>}/>
+    </Route>
+    </Routes>
+    </HashRouter>
+   
   </StrictMode>,
 )
--- vite-acm-2025/vite.config.ts
+++ vite-acm-2025/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ACM_Website_2025/',
+  base: '/ACM_Website_2025/#/',
 })
